aad86fa44646348c7cf390fb4b8dbec97a8e93bc
nogard <nogard@ttu.ee>
2020-04-13 06:59:19 +0300

    Beautify eriti loosely hääldada, siis kõlab nagu PewDiePie

--- HTML/templates/products.php
+++ HTML/templates/products.php
@@ -2,7 +2,7 @@
 require_once 'header.php';
 ?>
 <table class="products">
-    <tr><th>image</th><th>user</th><th>auction</th><th>description</th><th>startbid</th></tr>
+    <tr><th>image</th><th>user name</th><th>product</th><th>description</th><th>starting bid</th></tr>
     <?php
     foreach (getAuctions($db) as $row) {
         echo '<tr><td><img src="' . getFirstAuctionImage($row['id']) . '"/></td><td>' . $row['username'] . '</td><td>' . $row['title'] . '</td><td>' . $row['desc'] . '</td><td>' . $row['startingbid'] . '</td></tr>';
